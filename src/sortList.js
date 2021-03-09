d7899f0fdfc45efe3947908e660c8776c6474dc1
athenas2021 <lucas.kano@outlook.com>
2021-03-09 10:16:56 -0300

    Exercicio 1 sortList

--- src/sortList.js
+++ src/sortList.js
@@ -2,9 +2,17 @@
 /**
  * Recebe uma matriz e ordena ela pelo conteúdo.
  * Considere que virão apenas listas de números ou palavras.
- * @param array list 
+ * @param array list
  */
 function sortList(list){
+  if (!isNaN(list[0]){
+    list.sort(function(a, b) {
+    return a - b;
+    });
+  }
+  else{
+    list.sort();
+  }
 	return list;
 }
 
